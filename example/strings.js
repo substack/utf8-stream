d2dbca1f00709a01139005ba161c802cad156b78
James Halliday <mail@substack.net>
2013-07-19 05:03:09 -0700

    better strings example

--- example/strings.js
+++ example/strings.js
@@ -1,14 +1,15 @@
 var through = require('through');
 var utf8Stream = require('../');
 
-var buf = Buffer('☠ ￡');
+var u8 = utf8Stream();
+u8.pipe(through(function (buf) {
+    var s = buf.toString('utf8');
+    this.queue(s.split('').join('\n') + '\n');
+})).pipe(process.stdout);
 
-var d = utf8Stream();
-d.pipe(through(function (s) {
-    console.dir(s.toString('utf8'));
-}));
-
-for (var i = 0; i < buf.length; i++) {
-    d.write(buf.slice(i, i + 1));
-}
-d.end();
+u8.write(Buffer([ 0xe9, 0x81 ]));
+u8.write(Buffer([ 0x99, 0xe9, 0x81, 0xa0, 0xe6 ]));
+u8.write(Buffer([ 0x9c, 0xaa, 0xe4, 0xbe ]));
+u8.write(Buffer([ 0x86, 0xe7, 0x9a, 0x84, 0xe4, 0xba, 0x8b ]));
+u8.write(Buffer([ 0xe4, 0xbb, 0xb6 ]));
+u8.end();
